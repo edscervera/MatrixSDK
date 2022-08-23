99b5f354740282a7a27edc47ebaf934c3a044077
edscervera <100557247+edscervera@users.noreply.github.com>
2022-08-23 11:19:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MatrixSDK",
-            url: "https://github.com/edscervera/MatrixSDK/releases/download/0.23.15/MatrixSDK.xcframework.zip",
+            url: "https://github.com/edscervera/MatrixSDK/releases/download/v0.23.15/MatrixSDK.xcframework.zip",
             checksum: "e5a0004f44e740f26b876298379ee6cc682c8c85dc00d1288b6ae6af7e0a0c49"
         )
     ]
