a6225eaebc238be4dd68e814a1d60f08a5153889
Edson Cervera <ecervera@sorint.it>
2022-08-23 11:05:52 +0200

    Release v0.23.15

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MatrixSDK",
-            url: "https://github.com/edscervera/MatrixSDK/releases/download/v0.23.15/MatrixSDK.xcframework.zip",
-            checksum: "f6fb3973783c143da2d331df1d5fbfdd6639e81d4b192832385131ba8a48d911"
+            url: "https://github.com/edscervera/MatrixSDK/releases/download/0.23.15/MatrixSDK.xcframework.zip",
+            checksum: "e5a0004f44e740f26b876298379ee6cc682c8c85dc00d1288b6ae6af7e0a0c49"
         )
     ]
 )
